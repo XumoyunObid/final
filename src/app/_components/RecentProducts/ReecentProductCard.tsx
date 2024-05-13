a9fecdbbfd53a137f527e6cbcf665371e7de790c
Xumoyun <obidjonov06122005@gmail.com>
2024-05-13 13:36:08 +0500

    redux

--- src/app/_components/RecentProducts/ReecentProductCard.tsx
+++ src/app/_components/RecentProducts/ReecentProductCard.tsx
@@ -1,9 +1,11 @@
 "use client"
 import { Button } from "@/components/ui/button";
-import React from "react";
+import React, { useState } from "react";
 import { IoStarOutline } from "react-icons/io5";
-import { useDispatch } from "react-redux";
-import { addProduct } from "@/providers/redux/slices/product-slice";
+import { useDispatch, useSelector} from "react-redux";
+import { addProduct, removeProduct } from "@/providers/redux/slices/product-slice";
+import { RootState } from "@/providers/redux/store";
+import { FaShoppingCart } from "react-icons/fa";
 
 
 interface ImageItem {
@@ -12,32 +14,41 @@ interface ImageItem {
 }
 
 interface Props {
+  id: number;
   title: string;
   images: ImageItem[];
   price: string;
 
 }
 
-const RecentProductCard = ({
-  title,
-  images,
-  price
-}: Props) => {
-  const img =  images[0].image;
+const RecentProductCard = (
+  props
+: Props) => {
+  const {products} = useSelector((state:RootState)=> state.products)
+  console.log(products)
+  const img =  props.images[0].image;
   const dispatch = useDispatch()
+  const [showBtn, setShowBtn] = useState(false);
   const handleAddProduct = ()=> {
-    dispatch()
+    dispatch(addProduct(props))
+    setShowBtn(!showBtn)
+  }
+  const handleRemoveProduct = ()=> {
+    dispatch(removeProduct(props.id))
+    setShowBtn(!showBtn)
   }
   return (
-    <div className="p-[4px] lg:w-[226px] lg:h-[348px] flex flex-col bg-white gap-3 hover:shadow-lg">
-      <div className="flex items-center justify-center relative">
+    <div className="p-[4px] lg:w-[226px] lg:h-[348px] flex flex-col bg-white gap-3 hover:shadow-lg relative">
+      <div className="flex items-center justify-center">
         <img src={img} alt="" className="w-[216px] h-[216px] object-contain" />
-        <Button variant="secondary" className="rounded-full absolute top-0 right-0 px-3" onClick={handleAddProduct}>
-            <IoStarOutline className="hover:text-primary text-xl" />
-          </Button>
+        {!showBtn ? <Button variant="secondary" className="rounded-md absolute bottom-2 right-1 px-[10px] py-2 flex items-center justify-center bg-primary" onClick={handleAddProduct}>
+            <FaShoppingCart className="hover:text-primary text-xl" />
+          </Button> : <Button variant="secondary" className="rounded-md absolute bottom-2 right-1 px-[10px] py-2 flex items-center justify-center bg-black" onClick={handleRemoveProduct}>
+            <FaShoppingCart className="text-primary font-bold text-xl" />
+          </Button>}
       </div>
       <div className="flex flex-col gap-3 text-center">
-        <h2 className="text-primary text-sm">{title}</h2>
+        <h2 className="text-primary text-sm">{props.title}</h2>
         <div className="flex items-center gap-1 justify-center">
         <IoStarOutline/>
         <IoStarOutline/>
@@ -45,7 +56,7 @@ const RecentProductCard = ({
         <IoStarOutline/>
         <IoStarOutline/>
         </div>
-        <p className="text-bold text-xl">${price}</p>
+        <p className="text-bold text-xl">${props.price}</p>
       </div>
     </div>
   );
--- src/providers/redux/slices/product-slice.tsx
+++ src/providers/redux/slices/product-slice.tsx
@@ -2,10 +2,12 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface Product {
   id: number;
-  name: string;
-  price: number;
-  userCount: number;
-  userPrice: number;
+  images: {
+    image:string;
+    image_id:number;
+  }[]
+  title: string;
+  price: string;
 }
 
 interface ProductState {
@@ -24,27 +26,25 @@ const productSlice = createSlice({
   name: "products",
   initialState,
   reducers: {
-    totalPrice(state) {
-      const price = state.products.reduce(
-        (accumulator, currentValue) => accumulator + currentValue.userPrice,
-        0
-      );
-      state.totalPrice = price;
-    },
-    setCount(state) {
-      const count = state.products.reduce(
-        (accumulator, currentValue) => accumulator + currentValue.userCount,
-        0
-      );
-      state.count = count;
-    },
+    // totalPrice(state) {
+    //   const price = state.products.reduce(
+    //     (accumulator, currentValue) => accumulator + currentValue.userPrice,
+    //     0
+    //   );
+    //   state.totalPrice = price;
+    // },
+    // setCount(state) {
+    //   const count = state.products.reduce(
+    //     (accumulator, currentValue) => accumulator + currentValue.userCount,
+    //     0
+    //   );
+    //   state.count = count;
+    // },
     addProduct(state, action: PayloadAction<Product>) {
       const idf = state.products.find((item) => item.id === action.payload.id);
       if (!idf) {
         state.products.push({
-          ...action.payload,
-          userCount: 1,
-          userPrice: action.payload.price,
+          ...action.payload
         });
       }
     },
@@ -56,28 +56,28 @@ const productSlice = createSlice({
     removeAllProduct(state) {
       state.products = [];
     },
-    toggleAmount(state, action: PayloadAction<{ id: number; type: "ADD" | "REMOVE" }>) {
-      const { id, type } = action.payload;
-      const product = state.products.find((item) => item.id === id);
-      if (product) {
-        if (type === "ADD") {
-          product.userCount++;
-          product.userPrice = product.userCount * product.price;
-        } else if (type === "REMOVE" && product.userCount > 0) {
-          product.userCount--;
-          product.userPrice = product.userCount * product.price;
-        }
-      }
-    },
+    // toggleAmount(state, action: PayloadAction<{ id: number; type: "ADD" | "REMOVE" }>) {
+    //   const { id, type } = action.payload;
+    //   const product = state.products.find((item) => item.id === id);
+    //   if (product) {
+    //     if (type === "ADD") {
+    //       product.userCount++;
+    //       product.userPrice = product.userCount * product.price;
+    //     } else if (type === "REMOVE" && product.userCount > 0) {
+    //       product.userCount--;
+    //       product.userPrice = product.userCount * product.price;
+    //     }
+    //   }
+    // },
   },
 });
 
 export default productSlice.reducer;
 export const {
   addProduct,
-  setCount,
+  // setCount,
   removeProduct,
-  totalPrice,
-  toggleAmount,
+  // totalPrice,
+  // toggleAmount,
   removeAllProduct,
 } = productSlice.actions;
